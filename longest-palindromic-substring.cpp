aa069237e77b4333dcb576c4754ea7c33aba4367
onesafe <onesafe@163.com>
2014-09-15 20:18:08 +0800

    second huiwen commit

--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -5,90 +5,50 @@
 
 using namespace std;
 
-char *pre(const char *str){
-    int length = strlen(str);
-    char *prestr = new char[2*length + 4];
-    prestr[0] = '$';
-    prestr[1] = '#';
-    for(int i=0; i<length; ++i){
-        prestr[i*2 + 2] = str[i];
-        prestr[i*2 + 3] = '#';
+string longestPalindrome(string s){
+
+    int size = s.size(), i = 0;
+    char *t = new char[2*size + 2];
+    char *q = t;
+    int *p = new int[2*size + 2];
+    int mx = 0, id = 0, MAX = 0, center = 0;
+
+    for(*q='#',i=0; i<size; ++i,*++q='#'){
+        *++q = s[i];
     }
-    length = length*2 + 2;
-    prestr[length] = 0;
-    return prestr;
-}
 
-char *getMax(const char *str, int &maxlen){
-    char *prestr = pre(str);
-    int mx = 0, pi = 1;
-    int len = strlen(prestr);
+    for(*++q=0,p[0]=i=1; i<(2*size+2); i++){
+        p[i] = mx>i?min(p[2*id - i], mx-i):1;
 
-    int *p = new int[len];
-    p[0] = 0;
-
-    for(int i=1; i<len; i++){
-        
-        if(mx > i){
-            p[i] = min(mx-i, p[2*pi -i]);
-        }else{
-            p[i] = 1;
-        }
-
-        while(prestr[i-p[i]] == prestr[i+p[i]] && i-p[i]>0 && i+p[i]<len){
+        while(i+p[i] <= 2*size+1 && t[i+p[i]] == t[i-p[i]]){
             p[i]++;
         }
 
         if(i+p[i] > mx){
-            pi = i;
+            mx = i + p[i];
+            id = i;
+        }
+
+        if(p[i] > MAX){
+            MAX = p[i];
+            center = i;
         }
     }
 
-    int ret = 0;
-    for(int i=0; i<len; i++){
-        if(p[i] > maxlen){
-            maxlen = p[i];
-            ret = i;
-        }
-    }
+    delete(t);
+    delete(p);
 
-    int leftlen = ret - (maxlen - 1 );
-    int rightlen = ret + (maxlen - 1);
-    char *r = new char[rightlen - leftlen +1];
-    for(int i=0,j=leftlen; j<rightlen; ++i,++j){
-        r[i] = prestr[j];
-    }
-
-    delete []p;
-    delete []prestr;
-
-    return r;
+    return s.substr((center-MAX+1)/2, MAX-1);
 }
 
 int main()
 {
-    string s;
-    int ret;
-    int n = 0, j = 0;
-    char *res;
+    string r,s;
     cout<<"input a string: ";
     cin>>s;
-    res = getMax(s.c_str(), ret);
-    int len = strlen(res);
-    char *sub = new char(len);
-    for(int i=0; i<len; i++){
-        if(res[i] == '#'){
-            continue;
-        }else{
-            sub[j++] = res[i];
-        }
-    }
+    r = longestPalindrome(s);
 
-    sub[j] = '\0';
-    cout<<"substring length is: "<<ret-1<<endl;    
-    cout<<"substring is: "<<sub<<endl;
+    cout<<"substring is: "<<r<<endl;
 
-    delete []res;
-    delete []sub;
     return 0;
 }
